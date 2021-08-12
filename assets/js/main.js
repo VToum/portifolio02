c6ca28abaa9f104fbad9849196832161b041bb25
Vtm <everleao@gmail.com>
2021-08-12 15:35:56 -0300

    portifolio02

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,29 @@ const navMenu = document.getElementById('nav-menu'),
       navMenu.classList.remove('show-menu')
   }
   navLink.forEach(n => n.addEventListener('click', linkAction))
+
+  /*abrir e fechar as skills pelo traço do lado direito*/
+
+  /* selecionar classe pelo nome da classe no html*/
+  const skillsContent = document.getElementsByClassName('skills__content'),
+    /* selecionar todas classe pelo nome igual no html*/
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+    function toggleSkills(){
+      let itemClass = this.parentNode.className
+
+      /* Contador de ItemClass*/
+      for(i = 0; i < skillsContent.length; i++){
+        /*Array SkillsContent com contador para adicionar*/
+        skillsContent[i].className = 'skills__content skills__close'
+      }
+      /* se os items for igual as skills */
+      if(itemClass === 'skills__content skills__close'){
+        /* n.e.di */
+        this.parentNode.className = 'skills__content skills__open'
+      }
+    }
+        /* n.e.di */
+    skillsHeader.forEach((el) =>{
+      el.addEventListener('click',toggleSkills)
+    })
