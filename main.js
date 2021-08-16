2d53955ba7fd8adf4a5ac203dc1cc21efdb77fa4
Vtm <everleao@gmail.com>
2021-08-16 13:33:30 -0300

    qualificationFINAL

--- main.js
+++ main.js
@@ -52,3 +52,24 @@ const navMenu = document.getElementById('nav-menu'),
     skillsHeader.forEach((el) =>{
       el.addEventListener('click',toggleSkills)
     })
+
+    /* ======================= QUALIFICAÇÕES  TABS ================================ */
+
+    const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+      tabs.forEach(tab =>{
+        tab.addEventListener('click', () =>{
+          const target = document.querySelector(tab.dataset.target)
+
+          tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+          })
+          target.classList.add('qualification__active')
+
+          tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+          })
+          tab.classList.add('qualification__active')
+        })
+      })
